2f7f533dab2b62a3fd63c3cb4f4343a019cf0c4b
Andrea <a.ventura@students.ephec.be>
2020-02-06 12:15:05 +0100

    dossier programme de test interface et arduino pour comm avec pi ventura

---
+++ ProgrammesArduinoVentura/TestMouvPatteAvGau/TestMouvPatteAvGau.ino
@@ -0,0 +1,208 @@
+#include <SPI.h>
+#include <PID_v1.h>
+#include <Math.h>
+
+
+//------------------------------------------------------------------------
+#define LED_PIN   4
+#define APWM_PIN  5
+#define AEN_PIN   6
+
+#define BPWM_PIN  7
+#define BEN_PIN   8
+
+#define LOOPTIME  10   // loop time in ms
+
+#define TIMEOUT   500  // Timeout in ms
+
+#define DEADZONE  10    // /256 points  --- interval of confidence: +-10
+
+//-----------------------------------------------------------------------
+#define CS1 48 //Chip or Slave select  #1
+#define CS2 46 //Chip or Slave select  #2
+
+#define WAITING_MS 20 // time left by the master between reads
+
+#define WAITING_DS 150 // time left by the master between reads
+
+
+double T1_Off = 0;
+double T2_Off = 0;
+
+uint8_t temp1[1]; // contains the MSB of the position
+uint8_t temp2[1]; //  contains the LSB of the position
+
+double deg1 = 4.66;
+double deg2 = 186.06;
+
+bool motionEnabled = !false;
+bool LED_status = false;
+
+unsigned long s = 0;
+unsigned long lastTrans = 0; 
+
+unsigned long Dt = 0;
+unsigned long Mt = 0;
+unsigned long St = 0;
+
+uint16_t ABSposition1 = 0;  //  stores the Absolute position to be read from the encoder
+uint16_t ABSposition_last1 = 0; //  stores the last known position
+uint16_t ABSposition2 = 0;
+uint16_t ABSposition_last2 = 0;
+
+//-----------------------------------------------------------------------
+double theta21[] = {40, 42.3, 44.6, 46.9, 49.2, 51, 53.3, 55.6, 53, 50.5, 47.3, 45.0, 42.5, 41, 40.5 };
+double theta11[] = {85, 87.5, 90, 92.5, 95, 97.5, 100, 103, 100, 97.5, 95, 92, 89, 87, 85 };
+
+int tLength = 15;
+
+int pathIndex = 0;
+int pathIndex2 = 0;
+//------------------------------------    PID control variables --------------------------------------
+
+uint16_t maxAngleMargin = 0.5;
+double AJointSpd, BJointSpd;  // output variables to themotor
+double AJointAng, BJointAng;  // actual angular position of the joints received from the encoders
+double AJointSetpoint, BJointSetpoint, AJointSetpointOld, BJointSetpointOld;
+double previousAJointAng = AJointSetpoint;
+double previousBJointAng = BJointSetpoint;
+
+// Tuning parameters
+const int sampleRate = 1; //  sampling rate
+
+int k1 = 0.8*10 , k2 = 0.9*0.3/4, k3 = 0.9*4*0;
+
+double KpA=k1*3,  KpB=1.4*4;  //Initial Proportional Gain 
+double KiA=k3*10/10,  KiB=k3;  //Initial Integral Gain 
+double KdA=k2*5,  KdB=0.1*1;  //Initial Differential Gain 
+
+
+// Instantiate X and Y axis PID controls.
+PID MotorAPID(&AJointAng, &AJointSpd, &AJointSetpoint, KpA, KiA, KdA, DIRECT); 
+PID MotorBPID(&BJointAng, &BJointSpd, &BJointSetpoint, KpB, KiB, KdB, REVERSE); //setting up the PID
+
+
+void setup() {
+  pinMode(CS1,OUTPUT);//Slave#1 configuration 
+  pinMode(CS2,OUTPUT);//Slave#2 configuration 
+  digitalWrite(CS1,HIGH);  //  Slave#1 deSelected
+  digitalWrite(CS2,HIGH);  //  Slave#2 deSelected
+  
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setClockDivider(SPI_CLOCK_DIV64);
+  Serial.begin(9600);
+
+  Serial.println("starting");
+  Serial.flush();
+  Dt = millis();
+  
+  while(millis() < Dt+WAITING_DS);
+  Dt = millis();
+  SPI.end();
+  Mt = micros();
+
+
+  //------------------------------------------------------------------------
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(APWM_PIN, OUTPUT);
+  pinMode(AEN_PIN, OUTPUT);
+
+  pinMode(BPWM_PIN, OUTPUT);
+  pinMode(BEN_PIN, OUTPUT);
+
+  Serial.println("Dual Motor Leg Test : Start!");
+  
+  s = millis();
+  
+  //-------------------------------------------------------------------------
+  MotorAPID.SetMode(AUTOMATIC);
+  MotorAPID.SetSampleTime(sampleRate);
+  MotorAPID.SetOutputLimits(0,75);
+  
+  MotorBPID.SetMode(AUTOMATIC);
+  MotorBPID.SetSampleTime(sampleRate);
+  MotorBPID.SetOutputLimits(0,75);
+
+  // MotorAPID.Compute();
+  //reachAngle(82, 330);
+}
+
+void getPosition(){
+  uint8_t received = 0xA5;    //just a temp variable that the encoder will send if he's not ready of there isn't any remaining data to be sent
+   ABSposition1    = 0;    //reset position variable
+   ABSposition2 = 0;    //reset position variable
+   
+   SPI.begin();    //start transmition
+   
+   SPI_T(1,0x10);   //issue read command to encoder #1
+   received = SPI_T(1,0x00);    //issue NOP to check if encoder is ready to send
+   while (received != 0x10)    //loop while encoder is not ready to send 
+   {
+     received = SPI_T(1,0x00);    //check again if both encoders are still working 
+     //Serial.println("Encoder #1 not ready ! "); 
+     while(micros() < Mt+WAITING_MS);
+     Mt = micros();
+   }
+   
+   temp1[0] = SPI_T(1,0x00);    //Receive MSB
+   temp1[1] = SPI_T(1,0x00);    //Receive LSB
+   temp1[0] &=~ 0xF0;    //mask out the first 4 bits
+   
+   SPI_T(2,0x10);
+   received = SPI_T(2,0x00);
+   while (received != 0x10)
+   {
+     received = SPI_T(2,0x00);
+     //Serial.println("Encoder #2 not ready ! "); 
+     while(micros() < Mt+WAITING_MS);
+     Mt = micros();
+   }
+   
+   temp2[0] = SPI_T(2,0x00);
+   temp2[1] = SPI_T(2,0x00);
+   temp2[0] &=~ 0xF0;
+   
+   SPI.end();    //end transmition
+   
+   ABSposition1 = temp1[0] << 8;    //shift MSB to correct ABSposition in ABSposition message
+   ABSposition1 += temp1[1];    // add LSB to ABSposition message to complete message
+   ABSposition2 = temp2[0] << 8;
+   ABSposition2 += temp2[1];
+    
+   if (ABSposition1 != ABSposition_last1)    //if nothing has changed dont wast time sending position
+   {
+     ABSposition_last1 = ABSposition1;    //set last position to current position
+     deg1 = ABSposition1;
+     deg1 = deg1 * 0.08789;    // approx 360/4096
+
+     deg1 = map(deg1,0,360,360,0);
+
+     //Serial.println(deg1);     //send position in degrees
+   }else{
+    //Serial.println("Encoder #1 value received unchanged ! "); 
+   }
+   if (ABSposition2 != ABSposition_last2)
+   {
+     ABSposition_last2 = ABSposition2;
+     deg2 = ABSposition2;
+     //deg2 = (deg2 * 0.08789)%360;
+     //Serial.println(deg2);
+     deg2 = deg2 * 0.08789;
+
+     deg2 = map (deg2,360,0,0,360);
+
+   }else{
+    //Serial.println("Encoder #2 value received unchanged ! "); 
+   }
+
+   
+   AJointAng = deg1 ;
+   BJointAng = deg2 ;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
